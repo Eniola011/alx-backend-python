1ce43f45ec3bd6b170cbb63c53c07baaeb1a4a66
Eniola011 <yettyagbalu@gmail.com>
2024-03-15 07:44:11 +0100

    task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+  Add type-annotated function
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+        Addition of two float numbers.
+        Return the sum as float.
+    """
+    return a + b
