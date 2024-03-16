cb1b440b7b419e870bff86ad9c3f005e3b0093ab
Eniola011 <yettyagbalu@gmail.com>
2024-03-15 20:27:54 -0700

    float to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+  Float to String Type-Annotated Function
+"""
+
+
+def to_str(n: float) -> str:
+    """
+        Return the string representation of the float.
+    """
+    return str(n)
