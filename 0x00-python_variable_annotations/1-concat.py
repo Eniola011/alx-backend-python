fb15e43146360723fe88c696fb0eede3fbcd5232
Eniola011 <yettyagbalu@gmail.com>
2024-03-15 20:16:31 -0700

    concat two strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+  Concat Type-Annotated Function
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+        Return arguments as concatenated strings.
+    """
+    return str1 + str2
