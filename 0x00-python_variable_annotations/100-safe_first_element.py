282f6a95ed2fa8eba925baaf344fb1f5119912d2
Eniola011 <yettyagbalu@gmail.com>
2024-03-15 21:35:09 -0700

    Returns the first element of a list if it exists, otherwise returns None.

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+  Duck-Typed Annotation
+"""
+
+
+from typing import Any, Sequence, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """
+        Returns the first element of a list if it exists, otherwise returns None.
+    """
+    if lst:
+        return lst[0]
+    else:
+        return None
