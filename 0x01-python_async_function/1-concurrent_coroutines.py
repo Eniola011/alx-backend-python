92d40b298a0f56b4b105a54723496ce124efbf37
Eniola011 <yettyagbalu@gmail.com>
2024-03-18 06:41:32 -0700

    resolved: 1-concurrent_coroutines.py:18:16: E221 multiple spaces before operator

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -15,7 +15,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int = 0, max_delay: int = 10) -> List[float]:
     """ Concurrent_Coroutines """
     delays: List[float] = []
-    tasks: List  = []
+    tasks: List = []
 
     for _ in range(n):
         tasks.append(wait_random(max_delay))
